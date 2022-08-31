2000a058b4bbc4bb5ea6210fc5ed2afaaa4d075c
bruno-c-p <bruno.cardozo.pereira@gmail.com>
2022-08-31 15:08:18 -0300

    Implementando UI e criando os métodos do Board

--- src/main/java/br/com/brunocp/tictactoe/core/Board.java
+++ src/main/java/br/com/brunocp/tictactoe/core/Board.java
@@ -1,24 +1,76 @@
 package br.com.brunocp.tictactoe.core;
 
 import br.com.brunocp.tictactoe.Constants;
+import br.com.brunocp.tictactoe.ui.UI;
 
 public class Board {
 
-    char[][] matrix = new char[Constants.BOARD_SIZE][Constants.BOARD_SIZE];
+    char[][] matrix;
 
-    void clear() {
-
+    public Board() {
+        matrix = new char[Constants.BOARD_SIZE][Constants.BOARD_SIZE];
+        clear();
     }
 
-    void print() {
+    public void clear() {
 
+        for (int i = 0; i < matrix.length; i++) {
+
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                matrix[i][j] = ' ';
+            }
+        }
     }
 
-    boolean isFull() {
+    public void print() {
+
+        UI.printNewLine();
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                UI.printTextWithNoNewLine(String.valueOf(matrix[i][j]));
+
+                if (j < matrix[i].length - 1) {
+
+                    UI.printTextWithNoNewLine(" | ");
+                }
+            }
+
+            UI.printNewLine();
+
+            if (i < matrix[i].length - 1) {
+
+                UI.printText("---------");
+            }
+        }
+    }
+
+    public boolean isFull() {
+
+        for (int i = 0; i < matrix.length; i++) {
+
+            for (int j = 0; j < matrix[i].length; j++) {
+
+                if (matrix[i][j] == ' ') {
+
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    public boolean play(Player player, Move move) {
+
+        int i = move.i;
+        int j = move.j;
+
+        matrix[i][j] = player.symbol;
+
         return false;
-    }
-
-    void play() {
-
     }
 }
--- src/main/java/br/com/brunocp/tictactoe/core/Game.java
+++ src/main/java/br/com/brunocp/tictactoe/core/Game.java
@@ -1,6 +1,7 @@
 package br.com.brunocp.tictactoe.core;
 
 import br.com.brunocp.tictactoe.Constants;
+import br.com.brunocp.tictactoe.ui.UI;
 
 import java.util.Arrays;
 
@@ -11,5 +12,8 @@ public class Game {
 
     public void play() {
 
+        UI.printGameTitle();
+        board.print();
+//        UI.readInput("Nome do Jogador: ");
     }
 }
