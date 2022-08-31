dba062ad0cad595e5c5b671d32f79dcdef5619aa
bruno-c-p <bruno.cardozo.pereira@gmail.com>
2022-08-31 16:21:07 -0300

    Create Player e Next Player

--- src/main/java/br/com/brunocp/tictactoe/core/Game.java
+++ src/main/java/br/com/brunocp/tictactoe/core/Game.java
@@ -9,11 +9,38 @@ public class Game {
 
     private Board board = new Board();
     private Player[] players = new Player[Constants.PLAYER_SYMBOLS.length];
+    private int currentPlayerIndex = 0;
 
     public void play() {
 
         UI.printGameTitle();
         board.print();
-//        UI.readInput("Nome do Jogador: ");
+
+        for (int i = 0; i < players.length; i++) {
+            players[i] = createPlayer(i);
+        }
+    }
+
+    private Player createPlayer(int index) {
+
+        String name = UI.readInput("Jogador " + (index + 1) + " =>");
+
+        Player player = new Player(name, board, Constants.PLAYER_SYMBOLS[index]);
+
+        UI.printText("O jogador '" + player.getName() + "' vai usar o símbolo '" + Constants.PLAYER_SYMBOLS[index] + "'");
+
+        return player;
+    }
+
+    private Player nextPlayer() {
+
+        currentPlayerIndex++;
+
+        if (currentPlayerIndex >= players.length) {
+
+            currentPlayerIndex = 0;
+        }
+
+        return players[currentPlayerIndex];
     }
 }
